3eae299e7c14d538dcf94768cbf7aa41ff880217
Waleed <waleedthedev@gmail.com>
2021-12-31 01:02:12 +0500

    Basic nav setup

---
+++ components/Navbar.js
@@ -0,0 +1,11 @@
+export default function Navbar() {
+  return (
+    <nav className="flex justify-between">
+      <div className="flex">
+        <div>Logo</div>
+        <div>Huddle</div>
+      </div>
+      <button>Try It Free</button>
+    </nav>
+  )
+}
